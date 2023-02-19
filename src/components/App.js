9dffdcd181a6087967c0ea4d7612e41c2180fe6a
Vishal1732 <92910196+Vishal1732@users.noreply.github.com>
2023-02-19 17:26:35 +0530

    Create App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+// import '../styles/App.css';
 const App = () => {
+  const handleInput = (event) => {
+    // use console.log
+    console.log(`Input in #${event.target.id} is ${event.target.value}`);
+    // #text-input is sfdfg
+    // #num-input is 83247
+  };
 
-  const handleDoubleClick = (event) =>{
-  
-  }
+  // do not change id of input elements
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <label htmlFor="text-input">Text Input:- </label>
+      <input id="text-input" onChange={handleInput} type={"text"} />
+      <br />
+      <br />
+      <label htmlFor="num-input">Number input</label>
+      <input id="num-input" onChange={handleInput} type={"number"} />
+      <br />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
